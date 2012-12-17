f14ae703f7261eba68f03d3b07f08fbeeb9d3428
Randy Fischer <randy.fischer@gmail.com>
2012-12-17 01:12:35 -0500

    add support for direct dB setting, volume increment and decrement, mute and unmute

--- lib/volume-control.rb
+++ lib/volume-control.rb
@@ -6,8 +6,6 @@ class VolumeControl
     @vsx = vsx
   end
 
-  # to do: add incr, decr, set, mute, inquire methods
-
   def report
     return sprintf("%3.1f Db, %s", inquire_volume, inquire_mute ? 'muted' : 'not muted')
   end
@@ -21,59 +19,66 @@ class VolumeControl
   end
 
   def db= value
-    #####
+    code = if value >  12.0
+             '185VL' 
+           elsif value < -80.0
+             '000VL'
+           else
+             sprintf("%03dVL", (value/0.5 + 161).to_int)
+           end
+
+    @vsx.command(code)
+    resp = @vsx.persistent_command('?V', /^VOL(\d+)$/)
+    return nil unless resp
+    db =  decode_volume(resp[0])
+    return nil unless pretty_close(value, db)
+    return db
   end
 
   def mute
-    return if muted?
-    #####
+    return true if muted?
+    @vsx.command('MO')
+    return @vsx.persistent_command('?M', /^(MUT0)$/) == nil ? false : true
   end
 
   def unmute
-    return unless muted?
-    #####
+    return true unless muted?
+    @vsx.command('MF')
+    return @vsx.persistent_command('?M', /^(MUT1)$/) == nil ? false : true
   end
 
   def incr
-    #####
+    @vsx.command('VU')
+    resp = @vsx.persistent_command('?V', /^VOL(\d+)$/)
+    return decode_volume(resp[0]) if resp
+    return nil
   end
 
   def decr
-    #####
+    @vsx.command('VD')
+    resp = @vsx.persistent_command('?V', /^VOL(\d+)$/)
+    return decode_volume(resp[0]) if resp
+    return nil
   end
 
   private
 
+  def pretty_close x, y
+    (x - y).abs < 0.001
+  end
+  
+  def decode_volume code
+    return (code.to_i - 161) * 0.5
+  end
 
   def inquire_volume
     vol = @vsx.command_matches('?V', /^VOL(\d+)$/, 'volume inquiry')[0]
-    return (vol.to_i - 161) * 0.5
+    return decode_volume(vol)
   end
 
   def inquire_mute
     bool = @vsx.command_matches('?M', /^MUT([01])$/, 'mute inquiry')[0]
     return bool == '0' ? true : false
-  end
-
-
-  ##### FIXME
-
-  # increment up or down by increment, +1 = +0.5 dB
-
-  def volume_delta increment
-    increment = increment.to_i
-    return nil if increment == 0  # treat nil as no-op
-    increment < 0 ? ([ 'VD' ] * increment.abs).join("\r\n") : ([ 'VU' ] * increment).join("\r\n") 
-  end
-
-  ##### FIXME
-
-  # goes from -80.0 db to +12.db
-
-  def volume_set db
-    return '185VL' if db >  12.0
-    return '000VL' if db < -80.0  
-    return sprintf("%03dVL", (db/0.5 + 161).to_int)
   end
 
 end
